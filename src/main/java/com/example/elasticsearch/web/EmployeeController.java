1ea010f6a3a05f9b87d1094b28c4ef85c0683107
Katerina_Charakhovich <v-kcharakhovich@expediagroup.com>
2023-09-07 21:44:19 +0200

    cleanup

--- src/main/java/com/example/elasticsearch/web/EmployeeController.java
+++ src/main/java/com/example/elasticsearch/web/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.example.elasticsearch.web;
 
 import com.example.elasticsearch.model.Employee;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -13,10 +14,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping(path ="/employees", produces="application/json" )
+@RequestMapping(path = "/employees", produces = "application/json")
 public class EmployeeController {
 
     @Autowired
@@ -25,24 +29,31 @@ public class EmployeeController {
     @Autowired
     ObjectMapper objectMapper;
 
+    private static final String INDEX_SEARCH = "employees/_search";
+    private static final String INDEX_DOC = "employees/_doc/";
+    private static final String METHOD_GET = "GET";
+    private static final String METHOD_PUT = "PUT";
+    private static final String METHOD_DELETE = "DELETE";
+
+
     @GetMapping
     public ResponseEntity<?> findAllEmployees() {
         try {
-            Response response = restClient.performRequest(new Request("GET", "employees/_search"));
+            Response response = restClient.performRequest(new Request(METHOD_GET, INDEX_SEARCH));
             String responseBody = EntityUtils.toString(response.getEntity());
-            return ResponseEntity.ok(responseBody);
+            List<Employee> employees = convertToListEmployee(responseBody);
+            return ResponseEntity.ok(employees);
         } catch (IOException ex) {
             return ResponseEntity.badRequest().build();
         }
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> findEmployeeById(@PathVariable final String id){
+    public ResponseEntity<?> findEmployeeById(@PathVariable final String id) {
         try {
-            Response response = restClient.performRequest(new Request("GET", "employees/_doc/" + id));
+            Response response = restClient.performRequest(new Request(METHOD_GET, INDEX_DOC + id));
             String responseBody = EntityUtils.toString(response.getEntity());
-            final JsonNode node = objectMapper.readTree(responseBody);
-            Employee employee = objectMapper.readValue(node.get("_source").toString(), Employee.class);
+            Employee employee = convertToEmployee(responseBody);
             return ResponseEntity.ok(employee);
         } catch (IOException ex) {
             return ResponseEntity.badRequest().build();
@@ -54,13 +65,10 @@ public class EmployeeController {
         try {
             if (employee.getId() == null) {
                 employee.setId(UUID.randomUUID().toString());
-                return ResponseEntity.badRequest().build();
             }
-            Request request = new Request(
-                    "PUT",
-                    "employees/_doc/" + employee.getId());
+            Request request = new Request(METHOD_PUT,INDEX_DOC + employee.getId());
             request.setJsonEntity(objectMapper.writeValueAsString(employee));
-            restClient.performRequest(request);
+            Response response = restClient.performRequest(request);
             return ResponseEntity.ok().build();
         } catch (IOException ex) {
             return ResponseEntity.badRequest().build();
@@ -70,7 +78,7 @@ public class EmployeeController {
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteById(@PathVariable final String id) {
         try {
-            Response response = restClient.performRequest(new Request("DELETE", "employees/_doc/" + id));
+            Response response = restClient.performRequest(new Request(METHOD_DELETE, INDEX_DOC + id));
             int statusCode = response.getStatusLine().getStatusCode();
 
             return statusCode == 200
@@ -85,14 +93,12 @@ public class EmployeeController {
     public ResponseEntity<?> findBySearchParams(@RequestParam(name = "field") String field,
                                                 @RequestParam String value) {
         try {
-            String query = "{\"query\":{\"match\":{\"" +field+"\":\"" + value + "\"}}}";
-            Request request = new Request("GET", "employees/_search");
+            String query = "{\"query\":{\"match\":{\"" + field + "\":\"" + value + "\"}}}";
+            Request request = new Request(METHOD_GET, INDEX_SEARCH);
             request.setJsonEntity(query);
             Response response = restClient.performRequest(request);
             String responseBody = EntityUtils.toString(response.getEntity());
-            final JsonNode node = objectMapper.readTree(responseBody);
-            var employees = objectMapper.readValue(node.get("_source").toString(), Employee.class);
-                   // objectMapper.getTypeFactory().constructCollectionType(List.class, Employee.class));
+            List<Employee> employees = convertToListEmployee(responseBody);
             return ResponseEntity.ok(employees);
         } catch (IOException ex) {
             return ResponseEntity.badRequest().build();
@@ -105,9 +111,9 @@ public class EmployeeController {
                                                 @RequestParam(name = "metricField") String metricField) {
         try {
             String query = "{\"size\":0, " +
-                    "\"aggs\":{\"agg-by-skill\":{\"terms\":{\"field\":\"" +field+"\"}," +
-                    "\"aggs\":{\"metric\":{\""+metricType+"\":{\"field\":\""+metricField+"\"}}}}}}";
-            Request request = new Request("GET", "employees/_search");
+                    "\"aggs\":{\"agg-by-skill\":{\"terms\":{\"field\":\"" + field + "\"}," +
+                    "\"aggs\":{\"metric\":{\"" + metricType + "\":{\"field\":\"" + metricField + "\"}}}}}}";
+            Request request = new Request(METHOD_GET, INDEX_SEARCH);
             request.setJsonEntity(query);
             Response response = restClient.performRequest(request);
             String responseBody = EntityUtils.toString(response.getEntity());
@@ -115,5 +121,23 @@ public class EmployeeController {
         } catch (IOException ex) {
             return ResponseEntity.badRequest().build();
         }
+    }
+
+    private List<Employee> convertToListEmployee(String json) throws JsonProcessingException {
+        List<Employee> employees = new ArrayList<>();
+        final JsonNode node = objectMapper.readTree(json);
+        for (Iterator<JsonNode> it = node.get("hits").get("hits").elements(); it.hasNext(); ) {
+            JsonNode hitNode = it.next();
+            Employee employee = objectMapper.readValue(hitNode.get("_source").toString(), Employee.class);
+            String employeeId = hitNode.get("_id").toString();
+            employee.setId(employeeId);
+            employees.add(employee);
+        }
+        return employees;
+    }
+
+    private Employee convertToEmployee(String json) throws JsonProcessingException {
+        final JsonNode node = objectMapper.readTree(json);
+        return  objectMapper.readValue(node.get("_source").toString(), Employee.class);
     }
 }
